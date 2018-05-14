40db48d5f69b5a29f8961054027d27465c8818b7
intertonemu <intertonemu@intertonemu.emu>
2018-05-14 12:18:38 +0200

    pavo: something

---
+++ src/de/hsrm/cs/emu/interton/exception/CpuInvalidRegisterException.java
@@ -0,0 +1,7 @@
+package de.hsrm.cs.emu.interton.exception;
+
+public class CpuInvalidRegisterException extends Exception {
+
+	private static final long serialVersionUID = -2390892150406660198L;
+
+}
