9fd57f352b7bfc50571ebc1776891b22e8fee337
intertonemu <intertonemu@intertonemu.emu>
2018-05-28 09:13:35 +0200

    pavo: added StackMismatchException

---
+++ src/de/hsrm/cs/emu/interton/exception/CpuStackPointerMismatchException.java
@@ -0,0 +1,10 @@
+package de.hsrm.cs.emu.interton.exception;
+
+public class CpuStackPointerMismatchException extends Exception {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1138648198322752326L;
+
+}
